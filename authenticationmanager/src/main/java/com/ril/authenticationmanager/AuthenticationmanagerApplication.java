32611b7f514746ab2548992e04d97d2c81d8468e
ansh-93 <goyalaman1993@gmail.com>
2019-05-17 00:32:27 +0530

    Add files via upload

---
+++ authenticationmanager/src/main/java/com/ril/authenticationmanager/AuthenticationmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ril.authenticationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationmanagerApplication.class, args);
+	}
+
+}
